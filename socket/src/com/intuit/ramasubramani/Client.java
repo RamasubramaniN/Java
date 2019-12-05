a9911d7237a5f3083cd4556a65be0b125f81ab09
rn5 <ramasubramani_n@intuit.com>
2019-12-05 15:32:37 +0530

    Exploring Sockets in Java

---
+++ socket/src/com/intuit/ramasubramani/Client.java
@@ -0,0 +1,53 @@
+package com.intuit.ramasubramani;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.LinkedList;
+
+/**
+ * @author rn5
+ *
+ */
+public class Client {
+
+	public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+		LinkedList<String> messageList = new LinkedList<String>();
+		messageList.add("Hi");
+		messageList.add("How are you?");
+		messageList.add("Bye");
+		InetAddress localHost = InetAddress.getLocalHost();
+		
+		//The client must know the hostname or IP of the machine on which the server is running and the port 
+		//number on which the server is listening. In this case, We connect to the same client same port.
+		//In reality, Client ip address will be different and it will connect to server's ip address.
+		boolean stop = false;
+		while(!stop) {
+			Socket socket = new Socket(localHost.getHostAddress(), 6666);
+
+			// Send message to server
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+			objectOutputStream.writeObject(messageList.removeFirst());
+
+			// Receive Message from server
+			ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+
+			String line = (String) objectInputStream.readObject();
+			if(line.equals("Good night"))
+				stop = true;
+			System.out.println("Message from Server : " + line);
+			
+			objectOutputStream.close();
+			objectInputStream.close();
+
+			if (stop) {
+				socket.close();
+				break;
+			}
+	        Thread.sleep(1000);
+		}
+	}
+
+}
---
+++ socket/src/com/intuit/ramasubramani/Server.java
@@ -0,0 +1,64 @@
+package com.intuit.ramasubramani;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+
+/**
+ * @author rn5
+ *
+ */
+public class Server {
+
+	public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
+
+		LinkedList<String> messageList = new LinkedList<String>();
+		messageList.add("Hello");
+		messageList.add("I am good");
+		messageList.add("Good night");
+
+		// Creates an unbound server socket. Port Number 6666.
+		ServerSocket serverSocket = new ServerSocket(6666);
+		boolean stop = false;
+
+		while (!stop) {
+
+			// Listens for a connection to be made to this socket and accepts it.
+			// The method blocks until a connection is made. A new Socket is created
+			Socket socket = serverSocket.accept();
+			
+			// Receive Message from client
+			ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+
+			// Reads text from a character-input stream, buffering characters so as to
+			// provide for the
+			// efficient reading of characters, arrays, and lines.The buffer size may be
+			// specified,
+			// or the default size may be used. The default is large enough for most
+			// purposes.
+			String line = (String) objectInputStream.readObject();
+			if (line.equals("Bye"))
+				stop = true;
+			System.out.println("Message from Client : " + line);
+
+			// Send message to client
+			ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+			objectOutputStream.writeObject(messageList.removeFirst());
+			
+			objectOutputStream.close();
+			objectInputStream.close();
+
+			if (stop) {
+				stop = true;
+				socket.close();
+				serverSocket.close();
+				break;
+			}
+	        Thread.sleep(1000);
+		}
+	}
+
+}
